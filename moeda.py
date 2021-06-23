6be6120e4fb03193bcef14d3f5ffdf63c49fafc4
lsr.antonielle@gmail.com <lsr.antonielle@gail.com>
2021-06-23 19:58:29 -0300

    aperfeiçoando o código para formatar com base no parâmetro informado

--- moeda.py
+++ moeda.py
@@ -1,16 +1,29 @@
-def aumentar(n, porcento):
-    a = n * porcento / 100 + n
-    return a
+def aumentar(n=0, porcentagem=0, formato=False):
+    """
+    -> Função aumentar
+    :param n: Número a ser calculado
+    :param porcentagem: Porcentagem que o número vai sofrer à mais
+    :param formato: Define se existe formatação para o resultado ou não. Padrão é False
+    :return: Retorna o valor total do preço + a porcentagem aumentada
+    """
+    a = n * porcentagem / 100 + n
+    return a if formato is False else moeda(a)
 
 
+def diminuir(n=0, porcentagem=0,formato=False):
+    di = n - (n * porcentagem / 100)
+    return di if formato is False else moeda(di)
 
-def dobro(n):
-    d = n * 2
-    return d
+
+def dobro(n=0,formato=False):
+    do = n * 2
+    return do if not formato else moeda(do)
 
 
-def metade(n):
+def metade(n=0,formato=False):
     m = n / 2
-    return m
+    return m if not formato else moeda(m)
 
 
+def moeda(n = 0, moeda = 'R$'):
+    return f'{moeda}{n:>8.2f}'.replace('.',',')
--- sistemaPreco.py
+++ sistemaPreco.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Digite um preço: R$'))
-print(f'A metade de {p} é {moeda.m(p)}.')
-print(f'O dobro de {p} É {moeda.a(p)}.')
-print(f'Aumentando 10%, temos {moeda.a(p, 10)}.')
-print(f'Aumentando 13%, temos {moeda.a(p, 13)}.')
+print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}.')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}.')
+print(f'Aumentando 10% no valor de {moeda.moeda(p)} fica {moeda.aumentar(p, 10, True)}.')
+print(f'Diminuindo 13% no valor de {moeda.moeda(p)} fica {moeda.diminuir(p, 13, True)}.')
