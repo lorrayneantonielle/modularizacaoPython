8836860a6a38555f20f8db20a5734658138a1bfd
lsr.antonielle@gmail.com <lsr.antonielle@gail.com>
2021-06-23 20:09:50 -0300

    adicionando docstrings das funções

--- moeda.py
+++ moeda.py
@@ -11,19 +11,44 @@ def aumentar(n=0, porcentagem=0, formato=False):
 
 
 def diminuir(n=0, porcentagem=0,formato=False):
+    """
+    -> Função diminuir
+    :param n: Número a ser calculado
+    :param porcentagem: Porcentagem que o número vai sofrer à menos
+    :param formato: Define se existe formatação para o resultado ou não. Padrão é False
+    :return: Retorna o valor total do preço + a porcentagem de desconto
+    """
     di = n - (n * porcentagem / 100)
     return di if formato is False else moeda(di)
 
 
 def dobro(n=0,formato=False):
+    """
+    -> Função dobro
+    :param n: Número a ser calculado
+    :param formato: Define se existe formatação para o resultado ou não. Padrão é False
+    :return: Retorna o valor total do dobro do preço
+    """
     do = n * 2
     return do if not formato else moeda(do)
 
 
 def metade(n=0,formato=False):
+    """
+    -> Função metade
+    :param n: Número a ser calculado
+    :param formato: Define se existe formatação para o resultado ou não. Padrão é False
+    :return: Retorna o valor total da metade do preço
+    """
     m = n / 2
     return m if not formato else moeda(m)
 
 
 def moeda(n = 0, moeda = 'R$'):
+    """
+    -> Função moeda
+    :param n: Número a ser calculado
+    :param moeda: Define o valor atribuído
+    :return: Retorna a formtação escolhida para a moeda brasileira (R$)
+    """
     return f'{moeda}{n:>8.2f}'.replace('.',',')
